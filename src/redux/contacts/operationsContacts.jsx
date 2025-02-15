a257913a9ebefbdc3324a970ed99403eb35b4818
pinktulip24 <andreeak_09@yahoo.com>
2025-02-15 21:44:23 +0200

    Update operationsContacts.jsx

--- src/redux/contacts/operationsContacts.jsx
+++ src/redux/contacts/operationsContacts.jsx
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { toast } from 'react-toastify'; 
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
